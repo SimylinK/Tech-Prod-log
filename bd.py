306fc4ece2b21fdc14a302088ab46e83e164b370
SimylinK <maraval.nathan@gmail.com>
2016-02-29 11:58:53 +0100

    création tables

--- bd.py
+++ bd.py
@@ -1,43 +1,63 @@
 import mysql.connector as sql #mysql.connector 1.1.6
 from lireCSV import CSV_read
+from lireCSV import get_list_attribute
 
-def connect () :
-    conn = sql.connect(host="infoweb",user="E145910Y",password="E145910Y", database="E145910Y", port=3306)
-    cursor = conn.cursor()
-    conn.close()
+class BD:
+    cursor = None # variable de classe
+    conn = None # car cursor repose sur conn
 
-def create_table (CSV_file) :
-    list = CSV_read(CSV_file)
-    list_attribute = get_list_attribute(CSV_file)
+    def connect () :
+        try :
+            BD.conn = sql.connect(host="infoweb",user="E145910Y",password="E145910Y", database="E145910Y", port=3306)
+            BD.cursor = BD.conn.cursor()
+        except ValueError :
+            print("Connect error")
     
-
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS"""+remove_dot_CSV(CSV_file)+""" ( """ + 
-        for key, value in list_attribute :
-            key + " " +  python_type_to_SQL(value)
-        + """ ,); """)
-
-
-    #print (list)
-
-
-
-def define_PK (table_name, constraint_name, PK) :
-    cursor.execute("ALTER TABLE "+table_name+" ADD CONSTRAINT "+constraint_name+" PRIMARY KEY("+PK+");")
+    def close () :
+        try :
+            BD.cursor.close();
+        except ValueError:
+            print("Close error")
     
-def python_type_to_SQL(type) :
-    if type is "int" :
-        return "INTEGER DEFAULT NULL"
-    elif type is "str" :
-        return "VARCHAR(50) DEFAULT NULL"
-    elif type is "bool" :
-        return "BOOLEAN DEFAULT NULL"
-    else :
-        print ("Problème de type")
+    def create_table (CSV_file) :
+        list = CSV_read(CSV_file)
+        list_attribute = get_list_attribute(CSV_file)
+    
+        request = "CREATE TABLE IF NOT EXISTS "+BD.remove_dot_CSV(CSV_file)+" ("
+        
+        for key, value in list_attribute.items() :
+            request = request + (key + " " +  BD.python_type_to_SQL(value) + ",")
+        
+        request = request[:-1]
+        request = request + ");"
+            
+        try :
+            BD.cursor.execute(request)
+        except ValueError :
+            print("Execute error")
+    
+    def define_PK (table_name, constraint_name, PK) :
+        BD.cursor.execute("ALTER TABLE "+table_name+" ADD CONSTRAINT "+constraint_name+" PRIMARY KEY("+PK+");")
+        
+    def python_type_to_SQL(type) :
+        if type is "int" :
+            return "INTEGER DEFAULT NULL"
+        elif type is "str" :
+            return "VARCHAR(50) DEFAULT NULL"
+        elif type is "bool" :
+            return "BOOLEAN DEFAULT NULL"
+        else :
+            print ("Problème de type")
+    
+    def remove_dot_CSV (CSV_file_name) :
+        return CSV_file_name[:-4]
+        
 
-def remove_dot_CSV (CSV_file_name) :
-    return CSV_file_name[:-4]
+## main
 
-
-
-print (create_table("installations.csv"))
+BD.connect()
+BD.create_table("installations.csv")
+BD.create_table("equipements.csv")
+BD.create_table("activites.csv")
+#todo : définir clé primaire
+BD.close()
