70c2d7a0e7977df982ee62e0554e816e978be778
SimylinK <maraval.nathan@gmail.com>
2016-03-21 11:13:25 +0100

    Mise en place des appels ajax en js

--- controleur/server.py
+++ controleur/server.py
@@ -1,7 +1,7 @@
 import os.path
 import sys
 #pour revenir à un nvieau au-dessus dans le path (on fait un chemin absolu de celui où on est et on revient en plus un cran en arrière avec ..)
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from bottle import route, run, template, static_file
 from modele.bd import BD
 
@@ -16,15 +16,11 @@ def index(name):
 @route('/<filepath:path>')
 def index(filepath):
   return static_file(filepath,root=".")
-  
+
 @route('/import_install')
 def import_install() :
     BD.connect()
     BD.create_table("installations.csv")
     BD.close()
-  
+
 run(host='localhost', port=8080) # après avoir lancé le script, aller à l'adresse http://localhost:8080/hello/world
-
-
-
-
