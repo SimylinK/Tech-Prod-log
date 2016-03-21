7186f38cb3bc62b3dcd494800a93a61355c25383
jb lacour <dsijb@live.fr>
2016-03-21 13:13:38 +0100

    imports fonctionnels

--- controleur/server.py
+++ controleur/server.py
@@ -2,7 +2,7 @@ import os.path
 import sys
 #pour revenir à un niveau au-dessus dans le path (on fait un chemin absolu de celui où on est et on revient en plus un cran en arrière puis dans modele avec ../modele)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../modele")))
-from bottle import route, run, template, static_file, post, get, response
+from bottle import route, run, template, static_file, post, get, response, redirect
 import json
 from bd import BD
 
@@ -13,31 +13,27 @@ def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 '''
 
-#pour atteindre le fichier index.html dans le dossier vue
-@route('/<filepath:path>')
-def index(filepath):
-    return static_file(filepath,root="../vue")
 
-##Import des tables
-
-'''
+##Importation des tables
 @route('/create/<table_name>')
 def import_table(table_name) :
     BD.create_table(table_name+".csv")
-'''  
-    
-@route('/create/installations')
-def import_table() :
-    #BD.create_table("installations.csv")
-    return template('<b>Hello</b>!')
+    redirect("http://localhost:8080/index.html")
     
 
-
+##Affichage des tables
 @route('/display/<table_name>')
 def view_table(table_name):
     #response.headers['Content-type'] = 'application/json'
     res = json.dumps(BD.get_json_from_db(table_name))
     return res
+    
 
+
+#pour atteindre le fichier index.html dans le dossier vue. D'après nos tests, il faut toujours mettre cette route en dernier dans le code
+@route('/<filepath:path>')
+def index(filepath):
+    return static_file(filepath,root="../vue")
+    
 
 run(host='localhost', port=8080) # après avoir lancé le script, aller à l'adresse http://localhost:8080/index.html
