b69375d85b42f61de79b37d640b82333c082f0ef
jb lacour <dsijb@live.fr>
2016-03-21 16:47:21 +0100

    màj jb

--- controleur/server.py
+++ controleur/server.py
@@ -1,7 +1,9 @@
+import os
 import os.path
 import sys
 #pour revenir à un niveau au-dessus dans le path (on fait un chemin absolu de celui où on est et on revient en plus un cran en arrière puis dans modele avec ../modele)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../modele")))
+import bottle
 from bottle import route, run, template, static_file, post, get, response, redirect
 import json
 from bd import BD
@@ -13,27 +15,58 @@ def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 '''
 
+#on doit utiliser les CORS pour éviter une erreur de Cross-Origin Request
+class Cors(object):
+    name = 'cors'
+    api = 2
+
+    def apply(self, fn, context):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+            if bottle.request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
+
+
+app = bottle.app()
+
 
 ##Importation des tables
-@route('/create/<table_name>')
+@app.route('/create/<table_name>')
 def import_table(table_name) :
     BD.create_table(table_name+".csv")
-    redirect("http://localhost:8080/index.html")
+    #il faudra essayer de récup url de la page précédente pour pouvoir rediriger dessus
+    #redirect("http://localhost:8080/index.html") 
     
 
 ##Affichage des tables
-@route('/display/<table_name>')
+@app.route('/display/<table_name>', method=['OPTIONS', 'GET'])
 def view_table(table_name):
-    #response.headers['Content-type'] = 'application/json'
+    response.headers['Content-type'] = 'application/json'
     res = json.dumps(BD.get_json_from_db(table_name))
     return res
     
 
-
+'''
 #pour atteindre le fichier index.html dans le dossier vue. D'après nos tests, il faut toujours mettre cette route en dernier dans le code
 @route('/<filepath:path>')
 def index(filepath):
     return static_file(filepath,root="../vue")
-    
+'''
 
-run(host='localhost', port=8080) # après avoir lancé le script, aller à l'adresse http://localhost:8080/index.html
+##Installation des Cors
+app.install(Cors())
+
+
+##Récupération de l'IP
+f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+ip=f.read()
+
+##Lancement du serveur
+run(host=ip, port=8080, debug=True) # après avoir lancé le script, aller à l'adresse http://localhost:8080/index.html
